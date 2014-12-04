ee1d8d6d6f909137bb18b844afbe27ffc10a58c6
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-03 17:52:56 -0800

    fixed code, but still buggy: wordsort2.rb

--- wordsort2.rb
+++ wordsort2.rb
@@ -1,16 +1,32 @@
-words = [];
-string = gets.chomp;
 
-while (string != '')
-    words << string;
-string = gets.chomp;
+str = gets.chomp
+words = []
+
+while str != '!'
+    words.push(str)
+    str = gets.chomp
+end
+
+=begin
+words.sort.each do |word|
+    puts word;
 end;
+=end
 
-words_size = words.size;
-
-words_copy = [];
-
-
-words.each do|word|
-puts word;
+i = 0
+j = 0
+while(i < words.size)
+    words2 = words;
+    smallest = words2[0];
+    while(j <words.size)
+        if(smallest >= words2[j] && words2[i] != '!')
+            smallest = words2[j];
+            words2[j] = '!';
+            j = j+1;
+        end;
+    end;
+    j = 0;
+    i = i+1;
+    
+    puts smallest;
 end;
